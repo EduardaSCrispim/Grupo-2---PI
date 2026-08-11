5991a1f419af1709e570def2912dad92e103e108
Hackered0 <eduarda.crispim@sptech.school>
2026-08-11 17:55:06 -0300

    Tirando os inserts das tabelas

--- Entrega-oficial/web-data-viz/src/database/script-tabelas.sql
+++ Entrega-oficial/web-data-viz/src/database/script-tabelas.sql
@@ -52,7 +52,3 @@ create table medida (
 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
 );
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
